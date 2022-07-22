2cd3751423f82a75fbef6b0285ce44cf0a85b8f3
warren yun <wyun2006@gmail.com>
2022-07-22 00:57:16 -0400

    init tailwind styles

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      rotate: {
+        '3': '3deg'
+      },
+    },
+    fontFamily: {
+      sans: ['Josefin Sans', 'sans-serif'],
+      slab: ['Josefin Slab', 'serif']
+    },
+
+  },
+  plugins: [],
+}
