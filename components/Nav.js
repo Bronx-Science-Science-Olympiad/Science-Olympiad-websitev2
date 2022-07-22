e8de1192d8d004300bae7d7f91e575ac05d6a2c3
warren yun <wyun2006@gmail.com>
2022-07-22 01:17:06 -0400

    tailwind and next is mid

---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <div className="bg-black font-sans font-bold flex items-center text-yellow-500 justify-center text-7xl px-16 py-8">
+      <h1>SCIENCE OLYMPIAD
+</h1>
+    </div>
+  );
+}
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
-      
+      <h1 className='font-slab'>hello</h1>
     </div>
   )
 }
